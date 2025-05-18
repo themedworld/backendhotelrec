0504b8e23dfd8928fc1768ec2dd1b4d048792995
Traii Mohamed Amin <99541159+themedworld@users.noreply.github.com>
2025-05-18 21:55:08 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-echo "Téléchargement de parsed.jl depuis Google Drive..."
-gdown --id 1ajxkOOkbF06HCIRR67bPxGKRgFoTTMsD -O parsed.jl
-
-echo "Lancement de l'application Flask avec Gunicorn (1 worker)..."
-exec gunicorn rec:app \
-  --bind 0.0.0.0:$PORT \
-  --workers 1 \
-  --threads 1 \
-  --timeout 120
-
-=======
-# Télécharge parsed.jl si absent
-if [ ! -f parsed.jl ]; then
-  echo "Téléchargement de parsed.jl depuis Google Drive..."
-  gdown https://drive.google.com/uc?id=1ajxkOOkbF06HCIRR67bPxGKRgFoTTMsD -O parsed.jl
-fi
-
-# Lance le serveur Flask
-exec gunicorn app:app --bind 0.0.0.0:$PORT
->>>>>>> 14f6219 (Ajout du fichier parsed.jl.zip)
-=======
-echo "==> Décompression de parsed.jl.zip..."
-unzip -o data/parsed.jl.zip -d data/
-
-echo "==> Lancement de l'application Flask"
-exec gunicorn rec:app --bind 0.0.0.0:$PORT --workers 1
->>>>>>> a59960c (Sauvegarde avant pull --rebase)
+echo "Extracting parsed.jl.zip..."
+unzip -o parsed.jl.zip
+echo "Starting the app..."
+python rec.py 
