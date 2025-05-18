b26c71663abf3bb4c4c8ae9b371f04883ef41a78
Traii Mohamed Amin <99541159+themedworld@users.noreply.github.com>
2025-05-18 22:14:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
 echo "Creating data directory..."
 mkdir -p data
 
-echo "Extracting parsed.jl.zip to data/..."
-unzip -o parsed.jl.zip -d data
+if [ -f data/parsed.jl.zip ]; then
+    echo "Extracting parsed.jl.zip to data/..."
+    unzip -o data/parsed.jl.zip -d data
+else
+    echo "❌ Erreur : fichier data/parsed.jl.zip introuvable !"
+    exit 1
+fi
 
 echo "Starting the backend..."
 python rec.py
